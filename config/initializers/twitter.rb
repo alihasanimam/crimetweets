b92340801f024bacf06bbc0f4f7a71da60b4f7d8
Ali Hasan Imam <ali@backspace.io>
2015-07-08 08:12:21 +0200

    Fix twitter config load error

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,1 +1,5 @@
-TWITTER_CONFIG = YAML.load_file(Rails.root.join('config', 'twitter.yml'))[Rails.env]
+#Load from YAML file in local machine
+# TWITTER_CONFIG = YAML.load_file(Rails.root.join('config', 'twitter.yml'))[Rails.env]
+
+#Load from ENV variable in production server
+TWITTER_CONFIG = ENV['TWITTER_CONFIG']
