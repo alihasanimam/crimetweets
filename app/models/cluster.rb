113a3aabe182d1fcdbd104a596b3ee4de0b2a07a
Ali Hasan Imam <ali@backspace.io>
2015-07-09 04:44:24 +0200

    Added k-means clustering support.

--- app/models/cluster.rb
+++ app/models/cluster.rb
@@ -17,4 +17,58 @@ class Cluster
     end
     return locations
   end
+
+  def self.k_means
+    centroids = [[56.087228, -108.651087], [37.321928, -82.283899], [50.194330, 11.407507], [22.456503, 82.071571], [-23.216328, 146.934853]]
+    5.times do
+      map = %Q{
+        function(){
+          var tweet = this;
+          var min_distance = {key: [0, 0], value: 360};
+          var mean = [0 , 0];
+          var count = 0;
+          this.tweet_place.bounding_box.coordinates[0].forEach(function(coordinates){
+            mean[0] += coordinates[0];
+            mean[1] += coordinates[1];
+            count++
+          });
+          mean[0] = mean[0]/count;
+          mean[1] = mean[1]/count;
+
+          #{centroids.to_s}.forEach(function(centroid){
+            var distance = Math.sqrt(Math.pow(centroid[0] - mean[0], 2) + Math.pow(centroid[1] - mean[1], 2));
+            if (distance < min_distance.value){
+              min_distance.key = centroid;
+              min_distance.value = distance;
+            }
+          });
+          emit(min_distance.key, {coordinates: mean, distance: min_distance.value, count: 1});
+        }
+      }
+
+      reduce = %Q{
+        function(key, values){
+          var count = 0;
+          var coordinates = [0, 0];
+          var distance = 0;
+          values.forEach(function(value) {
+            coordinates[0] += value.coordinates[0] * value.count;
+            coordinates[1] += value.coordinates[1] * value.count;
+            distance += value.distance * value.count;
+            count += value.count;
+          });
+          return {coordinates: [coordinates[0]/count, coordinates[1]/count], distance: distance/count, count: count};
+        }
+      }
+
+      @results = {}
+      Tweet.where(:tweet_place.ne => nil).limit(1000).map_reduce(map, reduce).out(inline: true).each do |result|
+        @results[result['_id']] = result['value']
+        centroids[centroids.index(result['_id'])] = result['value']['coordinates']
+      end
+      puts @results.inspect
+    end
+
+    return @results
+  end
 end
